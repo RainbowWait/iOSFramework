a0bb8c43ee8662f25f014a89e62900e023871425
小燕 郑 <1136200379@qq.com>
2019-07-05 14:55:52 +0800

    增加fr加framework

---
+++ iOS-Framework/iOS-Framework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-Framework
+//
+//  Created by mac on 2019/7/5.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS-Framework/iOS-Framework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-Framework
+//
+//  Created by mac on 2019/7/5.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
